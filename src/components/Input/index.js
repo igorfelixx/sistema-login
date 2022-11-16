434739db913baf43fa92a0abe35440c0ce34320a
IgorFelixx <igorfelix13302@gmail.com>
2022-11-15 22:43:55 -0300

    commited

---
+++ src/components/Input/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as C from './styles'
+
+export default function Input({type, placeholder, value, onChange}) {
+
+    return (
+        <C.Input value={value} onChange={onChange} type={type} placeholder={placeholder} />
+    )
+
+}
